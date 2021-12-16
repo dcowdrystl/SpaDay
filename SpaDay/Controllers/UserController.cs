4d73d4c616310ee5943455c67613fc3ab7bfa21d
Dan Cowdry <medtc411@gmail.com>
2021-12-16 08:47:04 -0800

    added user form and submit functionality

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*[HttpGet("/user/add")]*/
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match! Try again!";
+                ViewBag.userName = newUser.Username;
+                ViewBag.eMail = newUser.Email;
+                return View("Add");
+            }
+        }
+
+    }
+}
